cb5f622aa3f75682c81f7c9cc6d30b907433b299
basana20 <135259639+basana20@users.noreply.github.com>
2024-08-28 22:16:03 -0400

    Add files via upload
    added files

---
+++ 004-implementation/AzureDatabricks/Notebooks/hive-metadata-tables-check.sql
@@ -0,0 +1,42 @@
+-- Databricks notebook source
+show databases
+
+-- COMMAND ----------
+
+use health_care_hive_met
+
+-- COMMAND ----------
+
+show tables
+
+-- COMMAND ----------
+
+describe table hlt_slv_claims_managed
+
+-- COMMAND ----------
+
+describe table hlt_slv_disease_managed
+
+-- COMMAND ----------
+
+describe table hlt_slv_group_managed
+
+-- COMMAND ----------
+
+describe table hlt_slv_grpsubgrp_managed
+
+-- COMMAND ----------
+
+describe table hlt_slv_hospital_managed
+
+-- COMMAND ----------
+
+describe table hlt_slv_patient_managed
+
+-- COMMAND ----------
+
+describe table hlt_slv_subgroup_managed
+
+-- COMMAND ----------
+
+describe table hlt_slv_subscriber_managed
